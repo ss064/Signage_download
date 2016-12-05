4618e5269c94c2633ac0d1741712470420dbe9cc
ss064 <1318064.ss@sist.ac.jp>
2016-12-06 05:56:51 +0900

    video activity

--- app/src/main/java/com/example/suzukisusumu_sist/signage_download/DownloadActivity.java
+++ app/src/main/java/com/example/suzukisusumu_sist/signage_download/DownloadActivity.java
@@ -1,17 +1,48 @@
 package com.example.suzukisusumu_sist.signage_download;
 
+import android.app.DownloadManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DownloadActivity extends AppCompatActivity {
     TextView tv;
+    DownloadManager downloadManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
-        String urls = getIntent().getStringExtra("urls");
+
+        String[] urls = getIntent().getStringExtra("urls").split("\r\n");
         tv = (TextView)findViewById(R.id.textView2);
-        tv.setText(urls);
+        tv.setText(getIntent().getStringExtra("urls"));
+        VideoDownload(urls[0]);
+    }
+
+    private void VideoDownload(String s_url){
+        //URIを生成する
+        downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(s_url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        tv.setText(uri.getPath());
+        //DownloadManager.Request request = new DownloadManager.Request(uriBuilder.build());
+        //保存場所、形式、ファイル名を指定
+
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_MOVIES,"test.mp4");
+        request.setTitle("Test.mp4");
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+        request.setMimeType("video/mp4");
+        long id=downloadManager.enqueue(request);
+
+        DownloadManager.Query query = new DownloadManager.Query();
+        query.setFilterById(id);
+        Cursor cursor = downloadManager.query(query);
+        int idStatus = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS);
+        cursor.moveToFirst();
+        Log.d("DownloadManagerSample", cursor.getString(idStatus));
     }
 }
---
+++ app/src/main/java/com/example/suzukisusumu_sist/signage_download/DownloadReceiver.java
@@ -0,0 +1,35 @@
+package com.example.suzukisusumu_sist.signage_download;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class DownloadReceiver extends BroadcastReceiver {
+    public DownloadReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // ダウンロードが完了したら結果用アクティビティを表示
+        if (intent.getAction().equals(DownloadManager.ACTION_DOWNLOAD_COMPLETE)) {
+
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+            Toast.makeText(context, "Download Complete ID : " + id, Toast.LENGTH_LONG).show();
+
+            if (id != -1) {
+                Intent resultIntent = new Intent(context, VideoActivity.class);
+                resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                resultIntent.putExtra(DownloadManager.EXTRA_DOWNLOAD_ID, id);
+                context.startActivity(resultIntent);
+            }
+        } else {
+            // ダウンロードマネージャの通知領域をクリックした場合はメッセージ表示のみ
+            Toast.makeText(context, intent.getAction(), Toast.LENGTH_LONG).show();
+        }
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
